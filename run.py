f356e0d6cb209b7c07be87cea9700a66b1bc019e
John Berry <ulfmagnetics@gmail.com>
2020-05-03 20:10:55 -0400

    Add comments and tweak colors a bit

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ OUTPUT_DIR = 'tmp/'
 # TODO: determine tile size that works based on srcage size
 TILE_WIDTH = 6
 TILE_HEIGHT = 12
-BACKGROUND_COLOR = 160
+BACKGROUND_COLOR = 190
 FILL_COLOR = 255
 
 n = 1
@@ -22,13 +22,13 @@ for x in range(0, src.size[0], TILE_WIDTH):
         bounds = x, y, x + TILE_WIDTH, y + TILE_HEIGHT
         tile = src.crop(bounds)
 
-        avg = int(numpy.average(tile.getdata()))
-
-        # simplest approach: just fill the tile with the average color
-        #avgtile = Image.new('L', tile.size, color=avg)
-
+        # fill the destination tile with BACKGROUND_COLOR
         avgtile = Image.new('L', tile.size)
         data = numpy.full((TILE_WIDTH, TILE_HEIGHT), BACKGROUND_COLOR)
+
+        # fill a portion of the height of the tile with FILL_COLOR
+        # based on the average value of the source tile
+        avg = int(numpy.average(tile.getdata()))
         height = int(TILE_HEIGHT * (avg / 255))
         data[0:height-1] = FILL_COLOR
         avgtile.putdata(data.flatten())
